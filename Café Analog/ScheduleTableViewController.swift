490ff9d1ffc6af339a20d84c685e961df8f3825e
Anders Fischer-Nielsen <andersfischern@me.com>
2016-03-08 23:12:05 +0100

    Added title, remove separators.

--- Café Analog/ScheduleTableViewController.swift
+++ Café Analog/ScheduleTableViewController.swift
@@ -14,6 +14,7 @@ class ScheduleTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Schedule"
         fetch()
     }
     
